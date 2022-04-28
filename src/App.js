795b2bedab3264401822066b38b927bfa91f0be8
kuhakuni <afif.haidar@gmail.com>
2022-04-28 16:04:59 +0700

    feat: add routing to app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
 
 function App() {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<h1 className="text-3xl underline">Hello world!</h1>
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
+		<Routes>
+			<Route path="/" element={<Home />} />
+			<Route path="/about" element={<h1>About</h1>} />
+		</Routes>
 	);
 }
 
