944782ca7673a653c365be52ffe111d16a66f2be
kuhakuni <afif.haidar@gmail.com>
2022-05-05 22:44:15 +0700

    feat: add important section

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "./Navbar/Navbar";
 import Hero from "./Hero/Hero";
 import Layanan from "./Layanan/Layanan";
+import Artikel from "./Artikel/Artikel";
+import Important from "./Important/Important";
 const Home = () => {
+	useEffect(() => {
+		document.title = "SJW FILKOM | Saling Jaga Warga FILKOM";
+	});
 	return (
 		<>
 			<Navbar />
 			<Hero />
+			<Artikel />
 			<Layanan />
+			<Important />
 		</>
 	);
 };
---
+++ src/components/Important/Important.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Important = () => {
+	return (
+		<div className="bg-primary">
+			<div className="container py-16 flex flex-col items-center justify-center">
+				<div className="text-center text-white">
+					<h1 className="font-bold text-5xl mb-8">
+						Kenapa harus SJW FILKOM?
+					</h1>
+					<p className="font-semibold text-2xl">
+						Semua data yang kamu berikan kepada SJW FILKOM akan
+						dijamin keamanannya dan langsung dianalisis oleh
+						kemahasiswaan. Tenaga ahli yang berperan dalam SJW
+						FILKOM sudah terkualifikasi kemampuannya untuk
+						memberikan pelayanan terbaik kamu.
+					</p>
+				</div>
+			</div>
+		</div>
+	);
+};
+export default Important;
