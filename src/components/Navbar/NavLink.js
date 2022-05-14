04d603554327c1deedb24a9d0a6218cab327c005
kuhakuni <afif.haidar@gmail.com>
2022-05-14 20:19:33 +0700

    fix : fix navlink

--- src/components/Navbar/NavLink.js
+++ src/components/Navbar/NavLink.js
@@ -1,12 +1,32 @@
 import React from "react";
-import { Link } from "react-router-dom";
-
-const NavLink = ({ children, ...props }) => {
+import { HashLink as NavHash } from "react-router-hash-link";
+// import { NavLink } from "react-router-dom";
+const NavLink = ({ children, to }) => {
+	const scrollToElement = (element) => {
+		const elementOffset = element.offsetTop;
+		const offset = elementOffset - 50;
+		window.scrollTo({
+			top: offset,
+			behavior: "smooth",
+		});
+	};
 	return (
 		<li>
-			<Link className="text-primary font-semibold" {...props}>
+			<NavHash
+				smooth
+				to={`/${to}`}
+				scroll={(el) => scrollToElement(el)}
+				// className={(isActive) => {
+				// 	return `${
+				// 		isActive ? "active" : ""
+				// 	} text-primary font-semibold`;
+				// }}
+				// activeClassName="active"
+				className={`text-primary font-semibold`}
+				tabIndex="-1"
+			>
 				{children}
-			</Link>
+			</NavHash>
 		</li>
 	);
 };
