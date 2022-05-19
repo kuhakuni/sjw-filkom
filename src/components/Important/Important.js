cc65d77bf6adc7285d39f0642f7b0407482dc840
kuhakuni <afif.haidar@gmail.com>
2022-05-19 08:00:10 +0700

    add awan-awan

--- src/components/Important/Important.js
+++ src/components/Important/Important.js
@@ -1,20 +1,27 @@
 import React from "react";
-
+import cloudLeft from "../../assets/img/leftcloudred.svg";
+import cloudRight from "../../assets/img/rightcloudyellow.svg";
 const Important = () => {
 	return (
 		<div className="bg-primary">
-			<div className="container py-16 flex flex-col items-center justify-center">
-				<div className="text-center text-white">
-					<h1 className="font-bold text-5xl mb-8">
+			<div className="container py-16 flex flex-row gap-12 content-center justify-between ">
+				<div className="hidden md:block self-center w-full">
+					<img className="lg:w-3/4 mx-auto" src={cloudLeft} alt="" />
+				</div>
+				<div className="flex flex-col items-center justify-center text-center text-white">
+					<h1 className="font-bold text-xl pb-3 sm:text-xl md:text-3xl lg:text-4xl leading-5 mb-8">
 						Kenapa harus SJW FILKOM?
 					</h1>
-					<p className="font-semibold text-2xl">
+					<p className="font-semibold text-sm sm:text-lg md:text-xl">
 						Semua data yang kamu berikan kepada SJW FILKOM akan
 						dijamin keamanannya dan langsung dianalisis oleh
 						kemahasiswaan. Tenaga ahli yang berperan dalam SJW
 						FILKOM sudah terkualifikasi kemampuannya untuk
 						memberikan pelayanan terbaik kamu.
 					</p>
+				</div>
+				<div className="hidden md:block self-center w-full">
+					<img className="lg:w-3/4 mx-auto" src={cloudRight} alt="" />
 				</div>
 			</div>
 		</div>
