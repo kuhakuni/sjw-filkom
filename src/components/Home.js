8aebb80a8d24b4aca2202919ccb360198f0ccd7f
kuhakuni <afif.haidar@gmail.com>
2022-05-26 23:35:38 +0700

    fetch api

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,16 +3,34 @@ import Hero from "./Hero/Hero";
 import Layanan from "./Layanan/Layanan";
 import Artikel from "./Artikel/Artikel";
 import Important from "./Important/Important";
-import Footer from "./Footer/Footer";
 import Alur from "./Alur/Alur";
 import Logo from "./Logo/Logo";
 import Accordion from "./Faq/Accordion";
 
+async function getArtikel() {
+	const data = await fetch(
+		"https://api-instagram-bem-filkom-2022.herokuapp.com/sjw",
+		{
+			mode: "cors",
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+			},
+		}
+	)
+		.then((res) => res.json())
+		.then((data) => data);
+	return data;
+}
 
 const Home = () => {
+	const [artikel, setArtikel] = React.useState([]);
 	useEffect(() => {
+		getArtikel().then((data) => setArtikel(data));
 		document.title = "SJW FILKOM | Saling Jaga Warga FILKOM";
 	});
+
+	console.log(artikel);
 	return (
 		<>
 			<Hero />
@@ -22,7 +40,6 @@ const Home = () => {
 			<Alur />
 			<Accordion />
 			<Logo />
-			<Footer />
 		</>
 	);
 };
