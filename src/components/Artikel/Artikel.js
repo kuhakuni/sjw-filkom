7edeb6bccaa7320da05fe88aa934b4045f081d39
kuhakuni <afif.haidar@gmail.com>
2022-05-26 23:38:13 +0700

    fix artikel

--- src/components/Artikel/Artikel.js
+++ src/components/Artikel/Artikel.js
@@ -1,27 +1,21 @@
 import React from "react";
 import ArtikelCard from "./ArtikelCard";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 const Artikel = () => {
 	return (
 		<>
 			<div className="bg-secondary" id="artikel">
-				<div className="container py-16 flex flex-row  flex-wrap justify-around">
+				<div className="container py-16 grid sm:grid-cols-2 place-items-center lg:flex lg:justify-between gap-4 items-stretch">
 					<ArtikelCard />
 					<ArtikelCard />
 					<ArtikelCard />
-					<div className="bg-tertiary flex flex-col my-24 md:w-44">
-						<div className="">
-							<p className="text-left mx-auto text-3xl font-bold text-white w-3/4 my-24">
-								Lihat Kabar Lainnya!
+					<div className="bg-tertiary text-white w-full flex flex-col justify-center items-center">
+						<div className="p-10 text-center lg:text-left">
+							<p className="font-semibold text-3xl">
+								Lihat Artikel lainnya
 							</p>
 						</div>
-
-						<Link to="/">
-							<div className="text-8xl text-customYellow mx-10">
-								<ion-icon name="arrow-forward-circle-outline"></ion-icon>
-							</div>
-						</Link>
 					</div>
 				</div>
 			</div>
