d5530192362c244c833328f71bb653232b2a58c4
kuhakuni <afif.haidar@gmail.com>
2022-05-28 11:14:28 +0700

    Hide Artikel Section

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,45 +1,45 @@
-import React, { useEffect } from "react";
+import React from "react";
+import Navbar from "./Navbar/Navbar";
 import Hero from "./Hero/Hero";
 import Layanan from "./Layanan/Layanan";
-import Artikel from "./Artikel/Artikel";
+// import Artikel from "./Artikel/Artikel";
 import Important from "./Important/Important";
 import Alur from "./Alur/Alur";
-import Logo from "./Logo/Logo";
 import Accordion from "./Faq/Accordion";
 
-async function getArtikel() {
-	const data = await fetch(
-		"https://api-instagram-bem-filkom-2022.herokuapp.com/sjw",
-		{
-			mode: "cors",
-			method: "GET",
-			headers: {
-				"Content-Type": "application/json",
-			},
-		}
-	)
-		.then((res) => res.json())
-		.then((data) => data);
-	return data;
-}
+// async function getArtikel() {
+// 	const data = await fetch(
+// 		"https://api-instagram-bem-filkom-2022.herokuapp.com/sjw",
+// 		{
+// 			mode: "cors",
+// 			method: "GET",
+// 			headers: {
+// 				"Content-Type": "application/json",
+// 			},
+// 		}
+// 	)
+// 		.then((res) => res.json())
+// 		.then((data) => data);
+// 	return data;
+// }
 
 const Home = () => {
-	const [artikel, setArtikel] = React.useState([]);
-	useEffect(() => {
-		getArtikel().then((data) => setArtikel(data));
-		document.title = "SJW FILKOM | Saling Jaga Warga FILKOM";
-	});
+	// const [artikel, setArtikel] = React.useState([]);
+	// useEffect(() => {
+	// 	getArtikel().then((data) => setArtikel(data));
+	// 	document.title = "SJW FILKOM | Saling Jaga Warga FILKOM";
+	// });
 
-	console.log(artikel);
+	// console.log(artikel);
 	return (
 		<>
+			<Navbar />
 			<Hero />
-			<Artikel />
+			{/* <Artikel /> */}
 			<Layanan />
 			<Important />
 			<Alur />
 			<Accordion />
-			<Logo />
 		</>
 	);
 };
