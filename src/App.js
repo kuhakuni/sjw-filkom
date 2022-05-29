375bceddcff8dff2e8887fa29cb6264dbc12dc5a
kuhakuni <afif.haidar@gmail.com>
2022-05-29 17:50:42 +0700

    add scroll to top

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
 import { HashRouter } from "react-router-dom";
 import Home from "./components/Home";
 import Footer from "./components/Footer/Footer";
 import Logo from "./components/Logo/Logo";
 import Konseling from "./components/Konseling";
 
+const ScrollToTop = () => {
+	const { pathname } = useLocation();
+
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, [pathname]);
+
+	return null;
+};
+
 function App() {
 	return (
 		<HashRouter>
+			<ScrollToTop />
 			<Routes>
 				<Route exact path="/" element={<Home />} />
 				<Route path="/konseling" element={<Konseling />} />
 			</Routes>
 			<Logo />
 			<Footer />
+			{/* </ScrollToTop> */}
 		</HashRouter>
 	);
 }
