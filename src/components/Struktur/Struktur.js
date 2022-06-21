d977714c0a39df82550d44701fb2c2a5ba1536ab
unknown <tuffahati393@gmail.com>
2022-06-21 19:21:00 +0700

    alur

--- src/components/Struktur/Struktur.js
+++ src/components/Struktur/Struktur.js
@@ -7,8 +7,8 @@ const Struk = ()  => {
 	// 	background: `url(/assets/img/Struktur.png) center center / cover no-repeat`,
 	// };
     return(
-         <div className={`${styles.container} p-10 md:p-16`} id="struktur">
-             <h1 className="font-bold sm:text-lg md:text-xl lg:text-2xl xl:text-3xl md:pb-10"> Struktur Konselor Sebaya </h1>
+         <div className={`${styles.container} p-10 md:p-16 flex justify-center flex-col`} id="struktur">
+             <h1 className="font-bold sm:text-lg md:text-xl lg:text-2xl xl:text-3xl md:pb-10 "> Struktur Konselor Sebaya </h1>
              <img
                 className="pt-4 px-2 xsm:px-5 xsm3:px-10 xsm3:pt-6"
                 alt="missing struktur"
