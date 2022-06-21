7f8a302f501b7d20effc783ae413d1ad50de3a80
unknown <tuffahati393@gmail.com>
2022-06-21 20:20:27 +0700

    change scale alur

--- src/components/Alur/Alur.js
+++ src/components/Alur/Alur.js
@@ -6,22 +6,19 @@ const Alur = () => {
 			<div
 				className={`${styles.container}  pb-10 pt-12 px-6 sm:px-16 md:px-28 lg:px-40 xl:px-64`}
 			>
-				<h1 className="text-tertiary text-center font-bold text-xl pb-3 sm:text-xl md:text-3xl lg:text-4xl">
+				<h1 className="text-tertiary text-center font-bold text-xl pb-3 sm:text-xl md:text-3xl lg:text-5xl">
 					Alur Pendaftaran
 				</h1>
-				<p className="text-center text-primary font-semibold text-base md:text-lg lg:text-xl">
+				<p className="text-center text-primary font-semibold text-base md:text-xl lg:text-2xl">
 					Yuk, mulai buka suara sama masalah yang kamu alami
 				</p>
 
-				<div className="bg-primary py-8 pl-5 pr-6 mt-5 text-white font-semibold text-sm flex justify-center">
-					<div className="mr-4">
+				<div className="bg-primary py-12 mt-5 text-white font-semibold text-sm flex justify-center">
 					<img
 						src="/assets/img/alur.png"
-						className="flex place-self-center md:ml-12 scale-110 ml-6 md:scale-100"
+						className="flex place-self-center md:pl-12 scale-125 pl-16 md:scale-100"
 						alt="missing logo"
 					></img>
-
-					</div>
 				</div>
 			</div>
 		</>
